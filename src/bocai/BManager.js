e19c4d4badd1d6da875b61a9256af3980e997dcb
杨旗 <yangq06@mysoft.com.cn>
2019-11-24 21:43:22 +0800

    update

--- src/bocai/BManager.js
+++ src/bocai/BManager.js
@@ -1,11 +1,12 @@
 const puppeteer = require("puppeteer")
 const devices = puppeteer.devices
 const iPhone = devices["iPhone 6"]
-// import * as ActionTools from './utils/ActionTools'
+const ActionTools = require("./utils/ActionTools")
 
 class BManager {
   constructor() {
     this._init()
+    this._login = true
   }
 
   async _init() {
@@ -30,24 +31,39 @@ class BManager {
     const page0 = page[0]
     await page0.emulate(iPhone)
     await page0.goto(
-      "https://98100a.com/m/#/bjpk10?id=5519&code=mamlaft&name=摩纳哥飞艇&title=摩纳哥飞艇&Value=0.07753564755402653"
+      "https://98100a.com/m/#/",
+      // "https://98100a.com/m/#/bjpk10?id=5519&code=mamlaft&name=摩纳哥飞艇&title=摩纳哥飞艇&Value=0.07753564755402653",
+      {
+        timeout: 0
+      }
     )
-    await page0.waitFor("#betbtn")
+    console.log("start waiting")
+    await page0.waitFor("#betbtn", {
+      timeout: 0
+    })
+    console.log("end waiting")
+
+    // if (this._login) {
+    //   await page0.waitFor("")
+    // } else {
+    // }
     this._ftPage = page0
 
-    await this.ftGoGo({ indexes: [2, 3, 4, 6] })
+    await this.ftGoGo({ indexes: [2, 3, 4, 5, 6, 7, 8], numbers: [3], bet: 2 })
   }
 
   async ftGoGo(data) {
-    // {indexes: [], numbers: []}
+    // {indexes: [], numbers: [], bet: number}
+    // clear
+    await this.clearFt()
+
     data.indexes.forEach(async index => {
       try {
-        await this._ftPage.$$eval(
-          `.shortcut ul li:nth-child(${index})`,
-          function(dom) {
-            console.log(window, dom, 8888)
-          }
+        const result = await ActionTools.tapSelector(
+          this._ftPage,
+          `.shortcut ul li:nth-child(${index})`
         )
+        // console.log(result)
         // this._ftPage.tap()
         // await element.click()
       } catch (e) {
@@ -55,6 +71,79 @@ class BManager {
       }
       console.log("点击", index)
     })
+
+    data.numbers.forEach(async number => {
+      try {
+        await ActionTools.tapSelector(
+          this._ftPage,
+          `.select-ball-warpper .kuaijie a[data-id="${number - 1}"]`
+        )
+      } catch (e) {
+        console.log(e)
+      }
+    })
+
+    await this._ftPage.waitFor(3000)
+
+    await this.ftBet(data)
+
+    // bet
+    // for test
+    // setTimeout(() => {
+    //   console.log('clear')
+    //   this.clearFt()
+    // }, 5000)
+  }
+
+  async clearFt() {
+    await ActionTools.tapSelector(this._ftPage, ".footer .delete")
+  }
+
+  async ftBet(data) {
+    await ActionTools.touchEndSelector(
+      this._ftPage,
+      ".footer .import .betValue"
+    )
+    const numbers = (data.bet + "").split("").map(n => +n)
+    console.log("input numbers", numbers)
+    numbers.forEach(async num => {
+      if (num === 0) {
+        await ActionTools.touchEndSelector(
+          this._ftPage,
+          "#pluginKeyborad .table-number tr:nth-child(4) td:nth-child(2)"
+        )
+      } else {
+        const trChildIndex = Math.floor((num - 1) / 3) + 1
+        let tdChildIndex = ((num - 1) % 3) + 1
+        await ActionTools.touchEndSelector(
+          this._ftPage,
+          `#pluginKeyborad .table-number tr:nth-child(${trChildIndex}) td:nth-child(${tdChildIndex})`
+        )
+      }
+    })
+    // click bet button
+    await this._ftPage.waitFor(5000)
+    await ActionTools.touchEndSelector(this._ftPage, "#betbtn")
+    // check validate
+    const totalInDialog = +this._ftPage.$$eval(".BET_AMT", doms => {
+      return doms.innerText
+    })[0]
+    const total = data.indexes.length * data.numbers.length * data.bet
+    console.log("tttttt", totalInDialog, total)
+    await this._ftPage.waitFor(5000)
+
+    // confirm
+    if (totalInDialog === total && false) {
+      await ActionTools.touchEndSelector(
+        this._ftPage,
+        "#actionsheet .sure .btn-sure"
+      )
+      console.log("done")
+    } else {
+      await ActionTools.touchEndSelector(this._ftPage, "#keyborad-mask")
+      console.log("不吻合，退出")
+    }
   }
 }
+
 module.exports = BManager
--- src/bocai/utils/ActionTools.js
+++ src/bocai/utils/ActionTools.js
@@ -1,4 +1,4 @@
-export function triggerTap(el) {
+function triggerTap(el) {
   // 创建自定义事件
   const event = document.createEvent("Events")
   // 初始化事件，可冒泡，可取消
@@ -7,7 +7,59 @@ export function triggerTap(el) {
   el.dispatchEvent(event)
 }
 
-export function triggerTouchEvent(el, eventType) {
+async function tapSelector(page, selector) {
+  return await page.$$eval(selector, doms => {
+    function triggerTap(el) {
+      // 创建自定义事件
+      const event = document.createEvent("Events")
+      // 初始化事件，可冒泡，可取消
+      event.initEvent("tap", true, true)
+      // 分发事件
+      el.dispatchEvent(event)
+    }
+    if (doms.length > 0) {
+      triggerTap(doms[0])
+    }
+    return doms
+  })
+}
+// type: touchstart touchend
+async function touchEndSelector(page, selector) {
+  return await page.$$eval(selector, doms => {
+    function triggerTouchEvent(el, eventType) {
+      // 获取目标元素的坐标、大小
+      const rect = el.getBoundingClientRect()
+      // 构建touch对象
+      const touch = new Touch({
+        identifier: Date.now(),
+        target: el,
+        clientX: rect.left + rect.width / 2,
+        clientY: rect.top + rect.height / 2,
+        // 下面的都是W3C实验性质的API
+        radiusX: 2.5,
+        radiusY: 2.5,
+        rotationAngle: 10,
+        force: 0.5
+      })
+      // 构建TouchEvent
+      const touchEvent = new TouchEvent(eventType, {
+        cancelable: true,
+        bubbles: true,
+        touches: [touch],
+        targetTouches: [],
+        changedTouches: [touch]
+      })
+      el.dispatchEvent(touchEvent)
+    }
+
+    if (doms.length > 0) {
+      triggerTouchEvent(doms[0], "touchend")
+    }
+    return doms
+  })
+}
+
+function triggerTouchEvent(el, eventType) {
   // 获取目标元素的坐标、大小
   const rect = el.getBoundingClientRect()
   // 构建touch对象
@@ -31,4 +83,11 @@ export function triggerTouchEvent(el, eventType) {
     changedTouches: [touch]
   })
   el.dispatchEvent(touchEvent)
+}
+
+module.exports = {
+  tapSelector,
+  touchEndSelector,
+  triggerTap,
+  triggerTouchEvent
 }
